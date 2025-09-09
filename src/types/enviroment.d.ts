896c0f3bcbf522706ce71ac26dd515c66cb196df
mikek1337 <mikekegnlyew@gmail.com>
2025-09-09 13:23:04 +0300

    chore: updated enviroment variables

--- src/types/enviroment.d.ts
+++ src/types/enviroment.d.ts
@@ -5,5 +5,8 @@ declare namespace NodeJS {
         API_KEY_VALUE: string; 
         DATABASE_URL: string;
         SHADOW_DATABASE_URL: string;
+        JWT_SECRET: string;
+        JWT_EXPIRES_IN: number;
+        REFRESH_TOKEN_EXPIRES_IN: string;
     }
 }
