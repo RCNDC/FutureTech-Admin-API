9573327131692d5c3204e8b97aa444d7b5e17c41
mikek1337 <mikekegnlyew@gmail.com>
2025-09-19 03:52:41 +0300

    update endpoint

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(
     origin:
       process.env.NODE_ENV == 'production'
         ? process.env.FRONTEND_URL
-        : 'http://localhost:5173',
+        : 'https://futuretech-admin-dashboard.onrender.com/',
     allowedHeaders: ['Content-Type', 'x-api-key', 'Authorization'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
