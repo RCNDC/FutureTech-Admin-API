04272076be13abde241883ce9c39868903d1b6fb
mikek1337 <mikekegnlyew@gmail.com>
2025-09-22 13:23:40 +0300

    feat: add embassy, local company, NGO, and startup name fields to submission queries

--- prisma/sql/ngoSubmission.sql
+++ prisma/sql/ngoSubmission.sql
@@ -7,7 +7,12 @@ WITH
         WHERE
             entry.form_id = 4783
     )
-SELECT t1.entry_id, t1.meta_value as 'fullName', t2.meta_value as email, t3.meta_value as 'phoneNo', t4.meta_value as 'registerAs', t1.date_created as 'registeredDate'
+SELECT t1.entry_id, 
+       t1.meta_value as 'fullName', 
+       t2.meta_value as email, 
+       t3.meta_value as 'phoneNo', 
+       t5.meta_value as 'orgName', 
+       t1.date_created as 'registeredDate'
 FROM
     cte_submissions as t1
     LEFT JOIN cte_submissions as t2 ON t1.entry_id = t2.entry_id
@@ -16,6 +21,8 @@ FROM
     and t3.meta_key = 'phone-1'
     LEFT JOIN cte_submissions as t4 ON t1.entry_id = t4.entry_id
     and t4.meta_key = 'select-7'
+    LEFT JOIN cte_submissions as t5 ON t1.entry_id = t5.entry_id
+    and t5.meta_key = 'name-3'
 WHERE
     t1.meta_key = 'name-1' AND (t4.meta_value = 'NGO or Foundation')
 GROUP BY
